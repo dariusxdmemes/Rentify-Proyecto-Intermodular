791c181a07abba85c861b4d56b6b2347bd321523
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-12-05 19:01:34 +0100

    Changed the TODO() call with a TODO message

--- app/src/main/java/com/example/rentify_proyecto_intermodular/ui/register/RegisterScreen.kt
+++ app/src/main/java/com/example/rentify_proyecto_intermodular/ui/register/RegisterScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -134,7 +135,7 @@ fun RegisterScreen(modifier: Modifier) {
                         )
                     }
 
-                    TODO() // IMPLEMENT REGISTER BUTTON
+                    // TODO implement Register button
                 }
             }
         }
