af85407bd53692f45e116ebf1b4e126f6ae2064d
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-12-12 20:37:35 +0100

    Fixed: Imported scroll behaviour

--- app/src/main/java/com/example/rentify_proyecto_intermodular/ui/register/RegisterScreen.kt
+++ app/src/main/java/com/example/rentify_proyecto_intermodular/ui/register/RegisterScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
