a8901f004001b833f0a7811f7a2dd7da60363a6c
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2026-02-24 16:09:04 +0100

    Implemented requests to update and delete a property.

--- app/src/main/java/com/example/rentify_proyecto_intermodular/data/api/propertyRequests.kt
+++ app/src/main/java/com/example/rentify_proyecto_intermodular/data/api/propertyRequests.kt
@@ -47,3 +47,59 @@ suspend fun registerProperty (property: Property) {
         throw IOException("Unexpected error")
     }
 }
+
+/**
+ * Sends a request to update the property in the database that has the same ID as the passed property, with the passed information.
+ * @param property The passed property to be updated.
+ * @throws IOException on network error
+ */
+suspend fun updateProperty (property: Property) {
+    withContext(Dispatchers.IO) {
+        val jsonBody = """
+            {
+               "id": "${property.id}",
+               "address": "${property.address}",
+               "ciudad": "${property.ciudad}",
+               "pais": "${property.pais}",
+               "alquiler": ${property.alquiler}
+            }
+        """.trimIndent()
+        val requestBody = jsonBody.toRequestBody(jsonMediaType)
+
+        val request = Request.Builder()
+            .url("http://$HOST:$PORT/property/update")
+            .put(requestBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) return@use
+            when (response.code) {
+                400 -> throw IOException("Missing field")
+                404 -> throw IOException("Property not found")
+                else -> throw IOException("Unexpected error")
+            }
+        }
+    }
+}
+
+/**
+ * Sends a request to delete a property from the database.
+ * @param propertyId The ID of the property to be deleted
+ * @throws IOException on network error
+ */
+suspend fun deleteProperty (propertyId: Int) {
+    withContext(Dispatchers.IO) {
+        val request = Request.Builder()
+            .url("http://$HOST:$PORT/property/$propertyId")
+            .delete()
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) return@use
+            when (response.code) {
+                404 -> throw IOException("Property not found")
+                else -> throw IOException("Unexpected error")
+            }
+        }
+    }
+}
